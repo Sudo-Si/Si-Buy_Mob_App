626b7c11aa62560be440f1d7d06c2dc4838c2d00
Sudo-Si <56238070+Sudo-Si@users.noreply.github.com>
2020-04-18 11:19:26 +1200

    Pages layout fixed

---
+++ Si_Buy/Activities/ItemActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Si_Buy.Activities
+{
+    [Activity(Label = "ItemActivity")]//, Theme ="AppTheme")]//, Theme = "AppTheme", MainLauncher = false)]
+    public class ItemActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.item_layout);
+            // Create your application here
+        }
+    }
+}
--- Si_Buy/Activities/MainActivity.cs
+++ Si_Buy/Activities/MainActivity.cs
@@ -5,6 +5,7 @@ using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using Si_Buy.Activities;
 
 namespace Si_Buy
 {
@@ -12,7 +13,7 @@ namespace Si_Buy
     public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
     {
         TextView textMessage;
-
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -22,6 +23,7 @@ namespace Si_Buy
             textMessage = FindViewById<TextView>(Resource.Id.message);
             BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
             navigation.SetOnNavigationItemSelectedListener(this);
+            //btnUpdateUser.Click += delegate { StartActivity(typeof(UpdateUserDetailsActivity)); };
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -34,17 +36,25 @@ namespace Si_Buy
             switch (item.ItemId)
             {
                 case Resource.Id.navigation_home:
+                  //  SetContentView(Resource.Layout.activity_main);
+                    StartActivity(typeof(MainActivity));
                     textMessage.SetText(Resource.String.title_home);
                     return true;
                 case Resource.Id.navigation_location:
+                  //  SetContentView(Resource.Layout.activity_main);
                     textMessage.SetText(Resource.String.title_location);
+                    StartActivity(typeof(LocationActivity));
                     return true;
                 case Resource.Id.navigation_profile:
+                    //SetContentView(Resource.Layout.profile_layout);
                     textMessage.SetText(Resource.String.title_profile);
+                    StartActivity(typeof(UserActivity));
                     return true;
 
                 case Resource.Id.navigation_Items:
+                    //SetContentView(Resource.Layout.item_layout);
                     textMessage.SetText(Resource.String.title_items);
+                    StartActivity(typeof(ItemActivity));
                     return true;
             }
             return false;
